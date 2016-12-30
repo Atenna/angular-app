18fd194e1557457c84086b9504b3fa12e6d78814
Ivana Lieska <idrabikova@gmail.com>
2016-12-30 14:35:01 +0100

    lunch checker

---
+++ module1/app.js
@@ -0,0 +1,38 @@
+/**
+ * Created by ATENNA on 30. 12. 2016.
+ */
+
+(function(){
+    'use strict';
+    angular.module('LunchCheck', [])
+       .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope']; // prevents minifiers from breaking the code
+
+    function LunchCheckController($scope) {
+        $scope.lunch = '';
+        $scope.result = '';
+        $scope.color = 'none';
+
+        $scope.calculate = function() {
+            var items = $scope.lunch.split(',');
+            $scope.length = items.length;
+            if(items.length < 4 ) {
+                if(items[0] === '') {
+                    $scope.result = ' Input data first ';
+                    $scope.color = 'red';
+                    $scope.border = 'red';
+                } else {
+                    $scope.result = ' Enjoy! ';
+                    $scope.color = 'green';
+                    $scope.border = 'green';
+                }
+            } else {
+                $scope.result = ' You eat too much! ';
+                $scope.color = 'green';
+                $scope.border = 'green';
+            }
+        }
+    }
+
+})();
